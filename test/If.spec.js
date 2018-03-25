947e9522dfe42684a7231c4b05e11cc2d938c187
Konstantin Simeonov <kon.simeonov@protonmail.com>
2018-03-25 21:39:55 +0300

    jsdom and innerHTML in da house

--- test/If.spec.js
+++ test/If.spec.js
@@ -4,18 +4,16 @@ const Tsc = require('./tsc/if-child-elements');
 const React = require('react');
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
-console.log(React);
 enzyme.configure({ adapter: new Adapter() })
 
 describe('If', () => {
     describe('renders child elements', () => {
         for(const shouldRender of [true, false]) {
             it(`does ${shouldRender ? 'not ' : ''} render child elements when condition={${shouldRender}}`, () => {
-                const babelWrapper = enzyme.shallow(React.createElement(Babel.IfChildElements, { shouldRenderLinks: shouldRender }));
-                const tscWrapper = enzyme.shallow(React.createElement(Tsc.IfChildElements, { shouldRenderLinks: shouldRender }));
+                const babelWrapper = enzyme.mount(React.createElement(Babel.IfChildElements, { shouldRenderLinks: shouldRender }));
+                const tscWrapper = enzyme.mount(React.createElement(Tsc.IfChildElements, { shouldRenderLinks: shouldRender }));
 
-                expect(babelWrapper.find('a')).to.have.lengthOf(shouldRender ? 2 : 0, 'babel fail');
-                expect(tscWrapper.find('a')).to.have.lengthOf(shouldRender ? 2 : 0, 'tsc fail');
+                expect(babelWrapper.getDOMNode().innerHTML).to.equal(tscWrapper.getDOMNode().innerHTML);
             });
         }
     });
---
+++ test/helpers/browser-env.js
@@ -0,0 +1,29 @@
+const { JSDOM } = require('jsdom');
+
+const { window } = new JSDOM(`
+	<!DOCTYPE html>
+	<html>
+		<head></head>
+		<body></body>
+	</html>
+`);
+
+function copyProperties(src, target) {
+	const propertiesToDefine = Object.getOwnPropertyNames(src)
+		.filter(propKey => typeof target[propKey] === 'undefined')
+		.reduce((propMap, propKey) => ({
+			...propMap,
+			[propKey]: Object.getOwnPropertyDescriptor(src, propKey)
+		}), {});
+
+	Object.defineProperties(target, propertiesToDefine);
+}
+
+const setupBrowserEnv = () => {
+	global.window = window;
+	global.document = window.document;
+	global.navigator = { userAgent: 'node.js' };
+	copyProperties(window, global);
+};
+
+setupBrowserEnv();
