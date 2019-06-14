480cb795adeb1d489489ba7c9f9cbb685850d93a
Konstantin Simeonov <konstantin.simeonov@chaosgroup.com>
2019-06-14 13:24:08 +0300

    Bump typescript to 3.5

--- runtests.sh
+++ runtests.sh
@@ -5,14 +5,15 @@ mkdir -p ./test-output
 yarn && yarn build
 yarn compile-compat-cases
 
-readonly TSC_VERSIONS=(2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.1 3.2 3.3 3.4 next)
+readonly TSC_VERSIONS=(2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.1 3.2 3.3 3.4 3.5 next)
 
 EXIT_CODE=0
 for v in ${TSC_VERSIONS[*]}; do
 #	rm -rf ./node_modules/typescript
-	echo "================= TESTING VERSION $v.* ======================="
+	echo "================= TESTING VERSION $v ======================="
 
 	yarn upgrade typescript@$v > /dev/null
+	yarn tsc --version
 	echo "Updated tsc to $v"
 
 	yarn test-compiled
@@ -20,7 +21,7 @@ for v in ${TSC_VERSIONS[*]}; do
 	CURRENT_EXIT_CODE=$?
 	[[ $CURRENT_EXIT_CODE != 0 ]] && EXIT_CODE=1
 
-	echo "Build and tests for $v.* exited with code $CURRENT_EXIT_CODE"
+	echo "Build and tests for $v exited with code $CURRENT_EXIT_CODE"
 done
 
 exit $EXIT_CODE
