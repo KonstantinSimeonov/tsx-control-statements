c67d9e3ce0557d5d3151144df9f5c1ab51a0d124
Konstantin Simeonov <konstantin.simeonov@chaosgroup.com>
2019-06-14 13:26:22 +0300

    Use --frozen-lockfile in runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -2,14 +2,13 @@
 
 mkdir -p ./test-output
 
-yarn && yarn build
+yarn --frozen-lockfile && yarn build
 yarn compile-compat-cases
 
 readonly TSC_VERSIONS=(2.4 2.5 2.6 2.7 2.8 2.9 3.0 3.1 3.2 3.3 3.4 3.5 next)
 
 EXIT_CODE=0
 for v in ${TSC_VERSIONS[*]}; do
-#	rm -rf ./node_modules/typescript
 	echo "================= TESTING VERSION $v ======================="
 
 	yarn upgrade typescript@$v > /dev/null
