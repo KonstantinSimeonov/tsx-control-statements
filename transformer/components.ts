c8538d518c803c82e6b49b37b176842f087ab38c
Konstantin Simeonov <kon.simeonov@protonmail.com>
2021-08-02 23:20:31 +0300

    Fix type error for 3.4, 3.5 and 3.6
    type JsxChild = (...) | JsxChild[]
    
    was wrong, since this type expression causes
    infinite nesting of array types and results in a
    type error

--- transformer/components.ts
+++ transformer/components.ts
@@ -4,34 +4,35 @@
  * compiler and various static analysis tools.
  */
 
-type JsxChild = string | number | boolean | JSX.Element | JsxChild[];
+type JsxChild = string | boolean | number | null | undefined | JSX.Element;
+type JsxChildren = JsxChild | JsxChild[];
 
-export function Choose(props: { children: JSX.Element[] }) {
+export function Choose(props: { children: JsxChildren }) {
   return props.children as any;
 }
 
-export function When(props: { children: JsxChild; condition: unknown }) {
+export function When(props: { children: JsxChildren; condition: unknown }) {
   return props.children as any;
 }
 
-export function If(props: { children: JsxChild; condition: unknown }) {
+export function If(props: { children: JsxChildren; condition: unknown }) {
   return props.children as any;
 }
 
-type NoBody<T> = { children?: JsxChild; each: string; of: Iterable<T>; index?: string };
+type NoBody<T> = { children?: JsxChildren; each: string; of: Iterable<T>; index?: string };
 type WithBody<T> = {
-  children?: JsxChild;
+  children?: JsxChildren;
   of: Iterable<T>;
-  body: (x: T, index: number) => JsxChild;
+  body: (x: T, index: number) => JsxChildren;
 };
 export function For<T>(props: NoBody<T> | WithBody<T>) {
+  return undefined as any;
+}
+
+export function Otherwise(props: { children: JsxChildren }) {
   return props.children as any;
 }
 
-export function Otherwise(props: { children: JsxChild }) {
-  return props.children as any;
-}
-
-export function With(props: { children: JsxChild; [id: string]: any }) {
+export function With(props: { children: JsxChildren; [id: string]: any }) {
   return props.children as any;
 }
--- transformer/runtests.sh
+++ transformer/runtests.sh
@@ -10,14 +10,15 @@ readonly supported_tsc_versions=(3.4 3.5 3.6 3.7 3.8 3.9 4.0 4.1 4.2 4.3 latest)
 exit_code=0
 for v in ${supported_tsc_versions[*]}; do
     echo "================= TESTING VERSION $v ======================="
-    yarn add --dev typescript@$v
+    git checkout ..
+    yarn add typescript@$v
     yarn tsc --version
     yarn test:compile
     yarn test:run
     test_exit_code=$?
     [[ $test_exit_code != 0 ]] && exit_code=1
 
-    echo "Build and tests for $v exited with code $CURRENT_EXIT_CODE"
+    echo "Build and tests for $v exited with code $test_exit_code"
 done
 
 exit $exit_code
