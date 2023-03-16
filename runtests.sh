449b2712f575d1cfaf6baaf23a374d75d4c2033e
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-03-16 17:52:53 +0200

    chore(ci): add 4.8 and 4.9 to ci

--- runtests.sh
+++ runtests.sh
@@ -4,13 +4,13 @@ mkdir -p test-output
 
 npm run build
 
-readonly supported_tsc_versions=(4.3 4.4 4.5 4.6 4.7)
+readonly supported_tsc_versions=(4.3 4.4 4.5 4.6 4.7 4.8 4.9)
 
 exit_code=0
 for v in ${supported_tsc_versions[*]}; do
     echo "================= TESTING VERSION $v ======================="
     git checkout ..
-    npm i typescript@$v
+    npm i --save-dev typescript@$v
     ./node_modules/.bin/tsc --version
     npm run test:compile
     npm run test:run
